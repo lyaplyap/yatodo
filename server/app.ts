c534ebe7080a5b8774988afbf8d8417a7f5a3d87
Nikita Liapin <lyapinne@gmail.com>
2023-11-07 11:50:47 +0300

    feat: added cors on server

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 
 import todosRoute from './routes';
 import { errorHandler } from './middlewares';
 
 const app: Express = express();
 
+app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
 app.use(express.json());
 
 // Routes
